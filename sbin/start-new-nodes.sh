5a8e42fbdad0e9b5f3265a7f7515b5b085581823
leventeliu <zhiteng.liu@thunderdb.io>
2018-04-21 05:16:35 +0800

    Add option to join the nodes

--- sbin/start-new-nodes.sh
+++ sbin/start-new-nodes.sh
@@ -7,6 +7,8 @@ declare -r IMAGE=regtest:latest
 declare -r BTC_PORT=18444
 declare -r RPC_PORT=18443
 
+declare JOIN=0
+declare JOINT=
 declare NODES=1
 declare START_PORT=5000
 
@@ -15,10 +17,18 @@ print_usage() {
 $0 [OPTION]...
 
     -h, --help              Display this help and exit
-    -n, --nodes             Set node number
-    -p, --start-port        Start port of a range
+        --join[=<ip:port>]  Join these new nodes into a BTC network; or to another network if the
+                              optional argument is given as a joint point
+    -n, --nodes=INT         Set node number
+    -p, --start-port=INT    Start port of a range
 
 USAGE
+}
+
+add_node() {
+    # $1: node name
+    # $2: target ip:port
+    sudo docker exec "$1" bitcoin-cli -datadir=data addnode "$2" add
 }
 
 start_new_nodes() {
@@ -45,13 +55,27 @@ start_new_nodes() {
         }
 
         _log_debug "New regtest node created: name=\"$name\" ip=\"$ip\" port=$BTC_PORT rpcport=$port"
+
+        if [[ "$JOIN" -eq 1 ]]; then
+            if [[ -z "$JOINT" ]]; then
+                # Use the first new node as a joint if none is given
+                JOINT="$ip:$BTC_PORT"
+            else
+                if add_node "$name" "$JOINT"; then
+                    _log_debug "Joined new node \"$name\" through \"$JOINT\""
+                else
+                    _log_error "Failed to join node \"$name\" to \"$JOINT\""
+                    return 1
+                fi
+            fi
+        fi
     done
 }
 
 main() {
     # Parse options
     local args
-    args=$(getopt -o hn:p: -l help,nodes:start-port: -n $0 -- "$@")
+    args=$(getopt -o hj::n:p: -l help,join::,nodes:start-port: -n $0 -- "$@")
     [[ $? -eq 0 ]] || { print_usage; return 1; }
     eval set -- "$args"
 
@@ -63,6 +87,11 @@ main() {
                 print_usage
                 [[ $argc -eq 1 ]] && { [[ $# -eq 1 ]] || [[ $# -eq 2 ]] && [[ $2 = "--" ]]; }
                 return
+                ;;
+            --join)
+                JOIN=1
+                JOINT="$2"
+                shift 2
                 ;;
             -n | --nodes)
                 NODES="$2"
